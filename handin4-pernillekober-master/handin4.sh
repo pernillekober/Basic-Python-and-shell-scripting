2b9949519c242a357e8dc6d6d75a43201e9a6ad0
pernillekober <46645201+pernillekober@users.noreply.github.com>
2022-09-19 14:46:20 +0200

    Delete handin4-pernillekober-master directory

--- handin4-pernillekober-master/handin4.sh
+++
@@ -1,9 +0,0 @@
-# Ex 1
-wget http://wouterboomsma.github.io/ppds2020/data/british-english
-wget http://wouterboomsma.github.io/ppds2020/data/british-english-subset
-
-# Ex 2
-diff british-english british-english-subset | cut -b 3 | uniq |sed '1d'
-
-# Ex 3
-grep -v '^[XYZ]' british-english > british-english-subset2
